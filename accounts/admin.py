92337539fe2256bbb9799c832defc82ec33d61d5
Vinicius Plumer <viniciusmplumer@gmail.com>
2022-10-31 15:33:44 -0300

    register user functionalities and homepage setup

---
+++ accounts/admin.py
@@ -0,0 +1,17 @@
+from .models import User, UserProfile
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    list_display = ('email', 'first_name', 'username', 'role', 'is_active')
+    ordering = ('-date_joined',)
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+
+
+admin.site.register(User, CustomUserAdmin)
+admin.site.register(UserProfile)
--- foodOnline_main/settings.py
+++ foodOnline_main/settings.py
@@ -27,6 +27,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'accounts',    
 ]
 
 MIDDLEWARE = [
@@ -73,6 +75,8 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = 'accounts.User'
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -108,11 +112,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS =  [
     'foodOnline_main/static'
 ]
+
+# Media files configuration
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
